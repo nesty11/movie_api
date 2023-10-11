f1d45cb1fbeee1d404f7215447b1d9484fb2b406
nesty11 <arielvale8@hotmail.com>
2023-10-11 17:29:46 -0400

    configured passport strategies

--- passport.js
+++ passport.js
@@ -1,9 +1,55 @@
-const passport = require('passport'),
-    LocalStrategy = require('passport-local').LocalStrategy,
-
-    Models = require('./models.js'),
-    passportJWT = require('passport-jwt');
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").LocalStrategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
 
 let Users = Models.User,
-    JWTStrategy = passportJWT.Strategy,
-    ExtractJWT = passportJWT.ExtractJWT;
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJWT;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      await Users.findOne({ Username: username })
+        .then((user) => {
+          if (!user) {
+            console.log("incorrect username");
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          console.log("finished");
+          return callback(null, user);
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+            return callback(error);
+          }
+        });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "your_jwt_secret",
+    },
+    async (jwtPayLoad, callback) => {
+      return await Users.findById(jwtPayLoad._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
