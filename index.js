5371fe0ab396b6678fa7a1dde5fd47cfcf85eb79
nesty11 <arielvale8@hotmail.com>
2023-10-31 18:31:38 -0400

    fixed typo and formatted code

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 mongoose.connect(process.env.CONNECTION_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const cors = require("cors");
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com", "http://localhost:1234"];
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+];
 app.use(
   cors({
     origin: (origin, callback) => {
@@ -28,7 +31,7 @@ app.use(
       if (allowedOrigins.indexOf(origin) === -1) {
         //If a specific origin isn't found on the list of allowed origins
         let message =
-          "The CORS policy for this application doesn't allow acces from origin" +
+          "The CORS policy for this application doesn't allow access from origin" +
           origin;
         return callback(new Error(message), false);
       }
@@ -66,19 +69,16 @@ app.get("/", (req, res) => {
 });
 
 //GET all movies 2.7
-app.get(
-  "/movies",
-  async (req, res) => {
-    await Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", async (req, res) => {
+  await Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 //GET movie info for one Title 2.7
 app.get(
