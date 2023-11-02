ac12dd4adbe91b1458c18cd29fa39135a331d7b8
nesty11 <arielvale8@hotmail.com>
2023-11-02 17:53:44 -0400

    re-enabled authentication

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ app.get("/", (req, res) => {
 //GET all movies 2.7
 app.get(
   "/movies",
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     await Movies.find()
       .then((movies) => {
