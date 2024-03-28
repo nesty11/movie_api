70ee1d89daffe4a9808b16d157c14ee2ba46d7a5
nesty11 <arielvale8@hotmail.com>
2024-03-27 21:58:40 -0400

    added test site

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
   "http://localhost:1234",
-  "https://nvflixapp.netlify.app"
+  "https://nvflixapp.netlify.app",
+  "http://localhost:4200",
 ];
 app.use(
   cors({
