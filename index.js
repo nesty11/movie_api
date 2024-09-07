1e6dd51a0f3ff6933a2e96aa26d531b2fcce4f19
nesty11 <114703822+nesty11@users.noreply.github.com>
2024-09-07 18:49:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,28 +48,29 @@ let allowedOrigins = [
   "http://localhost:4200",
   "https://nesty11.github.io/myFlix-Angular-App/welcome",
   "https://nesty11.github.io",
-  "https://final-static-bucket.s3-website-us-east-1.amazonaws.com",
+  "https://final-static-bucket.s3-website-us-east-1.amazonaws.com/",
+  "https://movieapi-2cmo.onrender.com/login"
 ];
 app.use(
   cors({
+    /**
+     * Check if the origin is allowed
+     * @param {string} origin - The origin of the request
+     * @param {function} callback - The callback function
+     */
     origin: (origin, callback) => {
-      if (!origin) return callback(null, true); // For non-browser requests
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        let message = `The CORS policy for this application doesn't allow access from the origin ${origin}`;
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        //If a specific origin isn't found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn't allow acces from origin" +
+          origin;
         return callback(new Error(message), false);
       }
+      return callback(null, true);
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
-
-// Handle preflight requests
-app.options('*', cors());
-
-app.use(express.json());
 
 let auth = require("./auth")(app);
 const passport = require("passport");
